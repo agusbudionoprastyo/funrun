7c376ce189bb378311bf2b37407d341273bb87c8
agusbudionoprastyo <agna4605@gmail.com>
2024-07-20 03:05:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -350,21 +350,21 @@
 			// Menambahkan iframe ke dalam body dokumen
 			document.body.appendChild(iframe);
 
-	function printSelectedEntries(entries) {
-    // Buat elemen iframe secara dinamis
-    var iframe = document.createElement('iframe');
-    // Tetapkan beberapa gaya untuk iframe
-    iframe.style.position = 'absolute';
-    iframe.style.left = '-9999px'; // Mengatur posisi di luar layar
-    iframe.style.width = '200mm'; // Menetapkan lebar iframe sesuai gaya label
-    iframe.style.height = '145mm'; // Menetapkan tinggi iframe sesuai gaya label
-    iframe.style.border = 'none'; // Menghapus border iframe
-    iframe.style.display = 'none'; // Sembunyikan iframe dari tampilan pengguna
-    document.body.appendChild(iframe);
+			function printSelectedEntries(entries) {
+			// Buat elemen iframe secara dinamis
+			var iframe = document.createElement('iframe');
+			// Tetapkan beberapa gaya untuk iframe
+			iframe.style.position = 'absolute';
+			iframe.style.left = '-9999px'; // Mengatur posisi di luar layar
+			iframe.style.width = '200mm'; // Menetapkan lebar iframe sesuai gaya label
+			iframe.style.height = '145mm'; // Menetapkan tinggi iframe sesuai gaya label
+			iframe.style.border = 'none'; // Menghapus border iframe
+			iframe.style.display = 'none'; // Sembunyikan iframe dari tampilan pengguna
+			document.body.appendChild(iframe);
 
-    var iframeDoc = iframe.contentWindow.document;
-    iframeDoc.open();
-    iframeDoc.write(`
+			var iframeDoc = iframe.contentWindow.document;
+			iframeDoc.open();
+			iframeDoc.write(`
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -471,6 +471,105 @@
         var qrCodeUrl = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + encodeURIComponent(entry.nomorBIB);
 
         iframeDoc.write(`
+		<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Cetak Entri - ${entries[0].namaGeng}</title>
+            <style>
+			@font-face {
+						font-family: 'Adumu'; /* Nama font yang akan digunakan */
+						src: url('assets/Adumu.ttf') format('truetype'); /* Lokasi file TTF */
+						/* Opsional: tambahkan format lain jika diperlukan */
+					}
+					body {
+						width: 200mm;
+						height: 145mm;
+						margin: 0;
+						padding: 0;
+						display: flex;
+						justify-content: center;
+						align-items: center;
+						position: relative;
+						font-weight: 700;
+						color: white;
+					}
+					.shape {
+						position: absolute;
+						top: 67%; /* Adjust vertically */
+						right: 7px;
+						transform: translate(-5%, -50%);
+						width: 80px;
+						height: 80px;
+						background-color: white;
+					}
+					.container {
+						position: relative;
+						width: 100%;
+						height: 100%;
+					}
+					.img,
+					.img-2,
+					.img-3 {
+						max-width: 100%;
+						height: auto;
+						display: block;
+						position: absolute;
+						left: 50%;
+						transform: translateX(-50%);
+					}
+					.img {
+						z-index: -1; /* Letakkan di belakang konten utama */
+					}
+					.img-2 {
+						top: 12mm; /* Adjust as needed */
+						width: 550px;
+					}
+					.img-3 {
+						bottom: 0; /* Adjust as needed */
+					}
+					.NameGroup {
+						position: absolute;
+						top: 50%; /* Adjust vertically */
+						left: 50%;
+						transform: translate(-50%, -50%);
+						text-align: center;
+						font-size: 88px;
+						font-family: 'Adumu';
+						line-height: 88px;
+						letter-spacing: 10px;
+					}
+					.headerTextLeft {
+						position: absolute;
+						top: 5%; /* Adjust vertically */
+						left: 5%;
+						transform: translate(-5%, -50%);
+						text-align: center;
+						font-size: 15px;
+						font-family: Arial, Helvetica, sans-serif;
+					}
+					.headerTextRight {
+						position: absolute;
+						top: 5%; /* Adjust vertically */
+						right: 5%;
+						transform: translate(5%, -50%);
+						text-align: center;
+						font-size: 15px;
+						font-family: Arial, Helvetica, sans-serif;
+					}
+					.BIBText {
+						position: absolute;
+						top: 73%; /* Adjust vertically */
+						left: 15px;
+						transform: translate(-5%, -50%);
+						text-align: center;
+						font-size: 45px;
+						font-family: 'Adumu';
+						letter-spacing: 5px;
+					}
+            </style>
+        </head>
+        <body>
             <div class="container">
 			<div class="shape">
 					<img src="${qrCodeUrl}" alt="QR Code" style="max-width: 100%; height: auto;">
@@ -483,13 +582,15 @@
 				<div class="BIBText">${entry.nomorBIB}</div>
 				<img src="assets/sponsor-bawah.png" class="img-3" alt="Image for printing">
             </div>
+			</body>
+        </html>
         `);
     });
 
-    iframeDoc.write(`
-        </body>
-        </html>
-    `);
+    // iframeDoc.write(`
+    //     </body>
+    //     </html>
+    // `);
 
     iframeDoc.close();
 
